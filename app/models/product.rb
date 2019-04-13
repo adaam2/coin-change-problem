2af8119bf71d298cb08e22119eb3b20cd0250949
Adam Bull <adamjamesbull@googlemail.com>
2019-04-13 14:37:19 +0100

    Implement a basic product model

---
+++ app/models/product.rb
@@ -0,0 +1,16 @@
+
+class Product
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  def to_h
+    {
+      name: name,
+      price: price
+    }
+  end
+end
